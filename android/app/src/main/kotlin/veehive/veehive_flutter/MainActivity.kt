0e5b6e0a51943550207ea5192a75de3764693eac
Landon <stahl.landon@gmail.com>
2021-01-14 22:25:07 -0800

    First commit

---
+++ android/app/src/main/kotlin/veehive/veehive_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package veehive.veehive_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Veehive.ai',
+      theme: ThemeData(
+        fontFamily: 'Karla',
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(title: 'Veehive.ai'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+                style: TextStyle(fontFamily: 'FjallaOne', fontSize: 20),
+              ),
+              Text(
+                '$_counter',
+                style: TextStyle(fontSize: 40),
+              ),
+              CircleAvatar(
+                radius: 73,
+                backgroundColor: Colors.transparent,
+                backgroundImage:
+                    AssetImage('assets/images/logoNoBackground.png'),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
